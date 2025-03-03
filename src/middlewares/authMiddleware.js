7590d163806e2405159dfaf2bc293648515eed74
kuldeepsinghra <19erecs035.kuldeep@rietjaipur.ac.in>
2025-03-03 10:34:57 +0530

    jwt token objects changes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 //middleware security using jwt token and verify the token
 exports.protect = (req, res, next) => {
-  const token = req.headers.authorization_?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
   console.log('token for validation', token);
   if (!token) {
     return res.status(401).json({ message: "Not authorized, token missing" });
